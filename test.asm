99cc9b0c07a511263dd0f660bbe743c99b37ddf4
Paval-from-Belarus <star-war-sandakan@mail.ru>
2022-12-29 01:17:26 +0300

    quick fixes

---
+++ test.asm
@@ -0,0 +1,18 @@
+;Input:
+;ds:dx -> something
+;Output:
+;es:ax -> another
+;idea -> tips with mov word <variable name>
+@@:
+
+    mov ax, cx
+    mov ax, 12
+    mov ax, 12 12
+    mov ax, 0x123
+    mov ax, $ad
+    mov ax, 0Fh
+    add ax, 123
+    mov word [pBuffer], 
+db 
+
+
